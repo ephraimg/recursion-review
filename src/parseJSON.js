765a6316f0b5ba496bdc75e17c4c1b0a30e5f6a8
ephraimg <ephraim.glick@gmail.com>
2017-10-10 20:54:27 -0700

    Begin parseJSON - basic vals, str, arr

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,77 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+
+  var at = 0;
+  var ch = json.charAt(at);
+  var next = function() { 
+    at++; 
+    ch = json.charAt(at);
+  };
+
+  // booleans & null
+  if (json === 'null') { return null; }
+  if (json === 'true') { return true; }
+  if (json === 'false') { return false; }
+
+  // number
+  if (!Number.isNaN(Number(json))) {
+    return Number(json);
+  }  
+
+  // string
+  if (json.startsWith('\"') && json.endsWith('\"')) {
+    var output = '';
+    next();
+    while (at < json.length - 1) {
+      if (ch === '\\') {
+        next();
+        if (ch === '\\') {
+          output += '\\';
+        }
+        if (ch === '\"') {
+          output += '\"';
+        }
+      } else {
+        output += ch;
+      }
+      next();   
+    }
+    return output;
+  }  
+
+  
+
+  var checkIfArray = function (str) {
+    var opens = [];
+    var closes = [];
+    if (str[0] === '[' && str[str.length - 1] === ']') {
+      for (var i = 0; i < str.length; i++) {
+        if (str[i] === '[') {
+          opens.push(str[i]);
+        } else if (str[i] === ']') {
+          closes.push(str[i]);
+        }
+      }
+      return (opens.length === closes.length);
+    }
+    return false;
+  };
+
+  
+
+  if (checkIfArray(json)) {
+    var output = [];
+    if (json.length > 2) {
+      var arrayInner = json.slice(1, json.length - 1);
+      arrayInner.split(',').forEach(function (el) {
+        el = el.trim();
+        output.push(parseJSON(el));
+    
+      });
+    }
+    //console.log(output);
+    return output;
+  }
+
 };
